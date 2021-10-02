37fe8435db527528592413fba735dcef7070cd33
xSolutionist <isac.hellstrom@iths.se>
2021-10-03 00:00:59 +0200

    Prepared communication system

---
+++ CommunicationSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommunicationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
