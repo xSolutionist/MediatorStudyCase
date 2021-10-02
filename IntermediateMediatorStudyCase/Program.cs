0465f7838b7672cbbe217de834ff09e6385eccc7
xSolutionist <isac.hellstrom@iths.se>
2021-10-02 15:51:10 +0200

    Added second project

---
+++ IntermediateMediatorStudyCase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntermediateMediatorStudyCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
